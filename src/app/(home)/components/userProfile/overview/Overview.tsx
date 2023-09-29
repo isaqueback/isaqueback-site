61e5ef25f9f8b76861915ca2a9de649dd0bd3df9
Isaque Delfino <isa.quecosta00@gmail.com>
2023-09-29 20:54:46 +0100

    removed next eslint extends

--- src/app/(home)/components/userProfile/overview/Overview.tsx
+++ src/app/(home)/components/userProfile/overview/Overview.tsx
@@ -3,32 +3,46 @@ import styles from './overview.module.sass'
 import H3 from '@/app/hTags/H3'
 
 export default function Overview() {
-    return (
-        <div className={` max-xl:bg-stone-100 flex flex-col gap-12 ${styles.overview} max-xl:w-full max-md:px-4 max-xl:py-10 max-lg:px-10 max-xl:px-14`}>
-            <H3 text="SOBRE MIM" />
-            <div className="max-xl:max-w-full max-w-[51rem] flex flex-col gap-10 mb-10 max-xl:mb-5 text-stone-800 font-light text-2xl max-xl:text-3xl">
-                <p>
-                    Olá! Sou um entusiasta apaixonado pela tecnologia, com um foco dedicado ao mundo do <strong>desenvolvimento web full stack</strong>.
-                    Minhas habilidades abrangem tanto o <strong>frontend</strong> quanto o <strong>backend</strong>, e estou totalmente comprometido em
-                    <strong> transformar ideias criativas em experiências digitais incríveis.</strong>
-                </p>
-                <p>
-                    Minha jornada na programação começou com a <strong>inspiração em Notch, o criador do Minecraft.</strong> Comecei explorando o desenvolvimento de jogos
-                    em Java e, ao longo do tempo, migrei para Python. Contudo, minha verdadeira paixão revelou-se no desenvolvimento web.
-                </p>
-                <p>
-                    Acredito que a <strong>aprendizagem é um processo contínuo</strong>, e estou sempre ansioso para adquirir novos conhecimentos e
-                    aprimorar minhas habilidades. Estou preparado para <strong>enfrentar desafios e criar soluções inovadoras.</strong>
-                </p>
-                <p>
-                    Ficaria muito contente em conversar com você e sua equipe sobre como minhas habilidades e paixão podem ser valiosas para o seu
-                    projeto ou empresa. Por favor, sinta-se à vontade para entrar em contato comigo a qualquer momento.
-                </p>
-            </div>
-            <nav className='flex gap-10 max-md:gap-10 max-lg:gap-20 max-md:flex-col max-lg:flex-row max-xl:flex-col max-md:items-center max-xl:justify-center'>
-                <Button1 text="CONTATO" href='#contacts' fontSize='lg' />
-                <Button1 text="PROJETOS" href='#projects' fontSize='lg' />
-            </nav>
-        </div >
-    )
+  return (
+    <div
+      className={` max-xl:bg-stone-100 flex flex-col gap-12 ${styles.overview} max-xl:w-full max-md:px-4 max-xl:py-10 max-lg:px-10 max-xl:px-14`}
+    >
+      <H3 text="SOBRE MIM" />
+      <div className="max-xl:max-w-full max-w-[51rem] flex flex-col gap-10 mb-10 max-xl:mb-5 text-stone-800 font-light text-2xl max-xl:text-3xl">
+        <p>
+          Olá! Sou um entusiasta apaixonado pela tecnologia, com um foco
+          dedicado ao mundo do <strong>desenvolvimento web full stack</strong>.
+          Minhas habilidades abrangem tanto o <strong>frontend</strong> quanto o{' '}
+          <strong>backend</strong>, e estou totalmente comprometido em
+          <strong>
+            {' '}
+            transformar ideias criativas em experiências digitais incríveis.
+          </strong>
+        </p>
+        <p>
+          Minha jornada na programação começou com a{' '}
+          <strong>inspiração em Notch, o criador do Minecraft.</strong> Comecei
+          explorando o desenvolvimento de jogos em Java e, ao longo do tempo,
+          migrei para Python. Contudo, minha verdadeira paixão revelou-se no
+          desenvolvimento web.
+        </p>
+        <p>
+          Acredito que a <strong>aprendizagem é um processo contínuo</strong>, e
+          estou sempre ansioso para adquirir novos conhecimentos e aprimorar
+          minhas habilidades. Estou preparado para{' '}
+          <strong>enfrentar desafios e criar soluções inovadoras.</strong>
+        </p>
+        <p>
+          Ficaria muito contente em conversar com você e sua equipe sobre como
+          minhas habilidades e paixão podem ser valiosas para o seu projeto ou
+          empresa. Por favor, sinta-se à vontade para entrar em contato comigo a
+          qualquer momento.
+        </p>
+      </div>
+      <nav className="flex gap-10 max-md:gap-10 max-lg:gap-20 max-md:flex-col max-lg:flex-row max-xl:flex-col max-md:items-center max-xl:justify-center">
+        <Button1 text="CONTATO" href="#contacts" fontSize="lg" />
+        <Button1 text="PROJETOS" href="#projects" fontSize="lg" />
+      </nav>
+    </div>
+  )
 }
--- src/app/(home)/components/userProfile/skills/Skills.tsx
+++ src/app/(home)/components/userProfile/skills/Skills.tsx
@@ -2,33 +2,35 @@ import H3 from '@/app/hTags/H3'
 import styles from './skills.module.sass'
 
 export default function Skills() {
-    return (
-        <div className={` flex flex-col gap-12 ${styles.skills} max-md:px-4 max-xl:py-10 max-lg:px-10 max-xl:px-14 max-w-full`}>
-            <H3 text="HABILIDADES" />
-            <ul className="flex flex-wrap max-xl:justify-center mt-2 gap-5 text-stone-700 text-2xl max-xs:text-xl max-xl:max-w-full max-w-[52.5rem]">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Node.js</li>
-            </ul>
-            <ul className="flex flex-wrap max-xl:justify-center mt-2 gap-5 text-stone-700 text-2xl max-xs:text-xl max-xl:max-w-full max-w-[52.5rem]">
-                <li>TypeScript</li>
-                <li>API REST</li>
-                <li>Git</li>
-                <li>GitHub</li>
-                <li>Design Responsivo</li>
-                <li>ORM Sequelize (PostgreSQL)</li>
-                <li>SASS</li>
-                <li>Tailwind</li>
-                <li>Multer (Amazon AWS)</li>
-                <li>Nodemailer</li>
-                <li>Trabalho em equipe</li>
-                <li>Pensamento crítico</li>
-                <li>Gestão do tempo</li>
-                <li>Resiliência</li>
-                <li>Ética profissional</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div
+      className={` flex flex-col gap-12 ${styles.skills} max-md:px-4 max-xl:py-10 max-lg:px-10 max-xl:px-14 max-w-full`}
+    >
+      <H3 text="HABILIDADES" />
+      <ul className="flex flex-wrap max-xl:justify-center mt-2 gap-5 text-stone-700 text-2xl max-xs:text-xl max-xl:max-w-full max-w-[52.5rem]">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+      <ul className="flex flex-wrap max-xl:justify-center mt-2 gap-5 text-stone-700 text-2xl max-xs:text-xl max-xl:max-w-full max-w-[52.5rem]">
+        <li>TypeScript</li>
+        <li>API REST</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Design Responsivo</li>
+        <li>ORM Sequelize (PostgreSQL)</li>
+        <li>SASS</li>
+        <li>Tailwind</li>
+        <li>Multer (Amazon AWS)</li>
+        <li>Nodemailer</li>
+        <li>Trabalho em equipe</li>
+        <li>Pensamento crítico</li>
+        <li>Gestão do tempo</li>
+        <li>Resiliência</li>
+        <li>Ética profissional</li>
+      </ul>
+    </div>
+  )
 }
