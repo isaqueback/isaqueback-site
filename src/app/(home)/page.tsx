c612588d644514cd2523ef565deefca20382803c
Isaque Delfino <isa.quecosta00@gmail.com>
2023-09-30 01:49:41 +0100

    added title and not-found

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from 'next'
 import Contacts from './components/contacts/Contacts'
 import Introduction from './components/introduction/Introduction'
 import Projects from './components/projects/Projects'
 import UserProfile from './components/userProfile/UserProfile'
+
+export const metadata: Metadata = {
+  title: 'Home | isaqueback'
+}
 
 export default function Home() {
   return (
