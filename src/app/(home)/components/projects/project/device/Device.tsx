ede6a7c0fa220c232382e6c4174a632ed5246610
Isaque Delfino <isa.quecosta00@gmail.com>
2023-09-30 01:22:27 +0100

    removed skeleton

--- src/app/(home)/components/projects/project/device/Device.tsx
+++ src/app/(home)/components/projects/project/device/Device.tsx
@@ -9,8 +9,6 @@ interface DeviceProps {
 }
 
 export default function Device({ img }: DeviceProps) {
-  const [reveal, setReveal] = useState(false)
-
   return (
     <div className="w-[31.25rem] max-xl:w-[40rem] max-2xl:w-[45rem] max-xs:w-72 min-h-[17.5rem] relative">
       <Image
@@ -18,13 +16,7 @@ export default function Device({ img }: DeviceProps) {
         alt="Imagem do site em diversos dispositivos"
         width={100}
         height={100}
-        onLoadingComplete={() => setReveal(true)}
-        className={`w-full h-full absolute ${reveal}`}
-      />
-      <Skeleton
-        variant="rounded"
-        animation="wave"
-        className={`w-[25rem] max-2xl:w-[25rem] max-xl:w-[20rem] max-lg:w-full  min-h-[17.5rem] absolute top-0 left-0 ${!reveal}`}
+        className={`w-full h-full`}
       />
     </div>
   )
